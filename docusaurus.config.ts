07dea572b1e3820ad70c90f569ac311cf1d1b8c8
Werner Raatz <werner.raatz@mail.de>
2026-07-13 19:41:15 +0200

    personalize copyright message

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -128,7 +128,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} Sven Patrick Meier (spmse). Built with Docusaurus and 💚.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Werner Raatz (raawer). Built with Docusaurus and extended from the developer-akademie-starter.`,
     },
     prism: {
       theme: prismThemes.github,
